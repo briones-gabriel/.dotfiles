5b4f51ea0b1328c46072faac77b4d9dd78ae8801
briones-gabriel <dossantosgabrielb@gmail.com>
2021-07-26 07:52:17 -0300

    Remove async startup from init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,19 +1,6 @@
 require "globals"
 require "settings"
 require "theme"
-
-local async
-
-async = vim.loop.new_async(
-  vim.schedule_wrap(
-    function ()
-      require "packerInit"
-      require "pluginsList"
-      require "maps"
-
-      async:close()
-    end
-  )
-)
-
-async:send()
+require "packerInit"
+require "pluginsList"
+require "maps"
