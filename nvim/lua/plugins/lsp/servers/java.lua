96dde2b4d44eb43ea11ceb94ac045c2ceb8267e4
briones-gabriel <dossantosgabrielb@gmail.com>
2021-07-28 08:31:57 -0300

    Fix java lang server

--- nvim/lua/plugins/lsp/servers/java.lua
+++ nvim/lua/plugins/lsp/servers/java.lua
@@ -1,14 +1,12 @@
 return function (lsp, on_attach, capabilities)
-  if pcall(lsp, "java") then
-    lsp.java.setup {
-      on_attach = on_attach,
-      capabilities = capabilities,
-      filetypes = { "java" },
-      init_options = {
-        jvm_args = {},
-        workspace = "/home/runner/workspace"
-      },
-      root_dir = lsp.util.root_pattern(".git"),
-    }
-  end
+  lsp.java.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "java" },
+    init_options = {
+      jvm_args = {},
+      workspace = "/home/runner/workspace"
+    },
+    root_dir = lsp.util.root_pattern(".git"),
+  }
 end
