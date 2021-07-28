d31b0db66a2818b7ce38c5a68e025eb80d1d61d8
briones-gabriel <dossantosgabrielb@gmail.com>
2021-07-27 22:26:36 -0300

    Changed the fold method

--- nvim/lua/plugins/nv-treesitter.lua
+++ nvim/lua/plugins/nv-treesitter.lua
@@ -1,20 +1,45 @@
 local present, _ = pcall(require, "nvim-treesitter")
 local present_2, _ = pcall(require, "nvim-treesitter.configs")
+
 if not (present or present_2) then
   return
 end
 
+local basic_languages = {
+  "html",
+  "vue",
+  "javascript",
+  "typescript",
+  "css",
+  "scss",
+  "java",
+  "sql",
+  "lua",
+  "php",
+  "json",
+  "jsdoc",
+  "comment",
+  "bash",
+  "tsx"
+}
+
 require("nvim-treesitter.configs").setup {
   autotag = {
-    enable  = true,
+    enable  = { "html", "vue" },
   },
   indent = {
-    enable  = true,
+    enable  = basic_languages,
   },
   autopairs = {
-    enable  = true,
+    enable  = basic_languages,
   },
   highlight = {
-    enable  = true,
+    enable  = basic_languages,
   },
 }
+
+-- nvim-treesitter folding
+vim.cmd([[
+  set foldmethod=expr
+  set foldexpr=nvim_treesitter#foldexpr()
+]])
--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -42,7 +42,6 @@ opt.fillchars       = {eob = " "}                                              -
 opt.shortmess:append("sIc")
 opt.shell           = "/bin/bash"
 opt.guicursor       = "i:block"
---opt.foldmethod      = "indent"
 opt.fileencoding    = "utf-8"                                                  --- The enconding written to a file
 opt.encoding        = "utf-8"                                                  --- The encoding displayed
 opt.colorcolumn     = "120"                                                    --- Sets the color column distance
