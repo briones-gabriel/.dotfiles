10b1d780c1888818482235cd26a6d92c4e43533f
briones-gabriel <dossantosgabrielb@gmail.com>
2021-08-30 16:03:53 -0300

    Fixed theme

--- config/.config/nvim/lua/theme.lua
+++ config/.config/nvim/lua/theme.lua
@@ -4,25 +4,20 @@ if not present then
   return
 end
 
---g.nord_contrast                  = false
---g.nord_borders                   = true
---g.nord_disable_background        = false
---g.nord_cursorline_transparent    = false
---g.nord_enable_sidebar_background = false
+local nightfox = require "nightfox"
 
---require("nord").set()
+nightfox.setup({
+  fox = "nordfox",
+  styles = {
+    comments = "italic",
+    keywods = "bold",
+    functions = "bold",
+  },
+  colors = {},
+  hlgroup = {}
+})
 
-g.nightfox_style = "nordfox"
-g.nightfox_terminal_colors = true
-g.nightfox_italic_comments = true
-g.nightfox_italic_functions = false
-g.nightfox_italic_keywords = false
-g.nightfox_italic_strings = false
-g.nightfox_italic_variables = false
---g.nightfox_color_delimiter = "red"
---g.nightfox_colors = {}
-
-require('nightfox').set()
+require('nightfox').load()
 
 vim.cmd([[
   syntax on
