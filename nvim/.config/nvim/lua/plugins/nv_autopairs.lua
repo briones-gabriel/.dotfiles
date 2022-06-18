c7eebe9bb0166b1c9eee616ea5312c195d51c94d
briones-gabriel <dossantosgabrielb@gmail.com>
2022-06-18 22:07:16 +0100

    Re-start neovim config

--- nvim/.config/nvim/lua/plugins/nv_autopairs.lua
+++
@@ -1,9 +0,0 @@
-require("nvim-autopairs").setup {
-  ts_config       = {
-    lua = {"string"},
-    javascript = {"template_string"},
-    java = false
-  },
-  check_ts        = true,
-  disable_filetype = { "TelescopePrompt" },
-}
